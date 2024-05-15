6b4c3e774f1e70ae194c7ce929bed5c7293d62a3
shakecoding <hogeun4656@gmail.com>
2024-05-16 00:29:50 +0900

    5월 15일 뷰 프로젝트 준비 시작

---
+++ workspace/4_vue-app/2_test/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 전역 컴포넌트 테스트 -->
+  <!-- <Checkbox /> -->
+
+  <!-- 지역 컴포넌트 테스트 -->
+  <h1>App 테스트</h1>
+  <ul>
+    <Checkbox />
+    <Checkbox />
+    <Checkbox />
+    <Checkbox />
+  </ul>
+</template>
+
+<script>
+import Checkbox from './components/Checkbox.vue';
+
+  // 이거 먼저 적으면 위에 거 자동완성 됨.
+  export default {
+    name : 'App',
+    components : {Checkbox}
+  }
+</script>
+
---
+++ workspace/4_vue-app/2_test/src/components/Checkbox.vue
@@ -0,0 +1,22 @@
+<!-- vbase-css -->
+<!-- vue vscode snippets -->
+
+<template>
+    <input type="checkbox" v-model="checked"> 체크박스 테스트
+    <br>
+</template>
+
+<script>
+    export default {
+        name : 'Checkbox',
+        data() {
+            return {
+                checked : false
+            }
+        },
+    };
+</script>
+
+<style scoped>
+
+</style>
