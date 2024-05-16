93567bc9f4db3e37171df338dcbe84de403ed3f0
hogeun <114096025+shakecoding@users.noreply.github.com>
2024-05-16 18:24:11 +0900

    5월 16일 추가!

---
+++ workspace/4_vue-app/3_prop-test/src/components/Checkbox.vue
@@ -0,0 +1,18 @@
+<!-- vbase-css -->
+<!-- vue vscode snippets -->
+
+<template>
+    <input type="checkbox" :checked='checked'> 체크박스 테스트
+    <br>
+</template>
+
+<script>
+    export default {
+        name : 'Checkbox',
+        props : ['name', 'checked']
+    };
+</script>
+
+<style scoped>
+
+</style>
