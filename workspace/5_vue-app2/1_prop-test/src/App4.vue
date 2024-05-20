ffd791166233d527e6ea172a21a6bb128d697a39
rhg4656 <llloe@naver.com>
2024-05-20 18:29:43 +0900

    5월 20일 수업 전 checkItem 추가

---
+++ workspace/5_vue-app2/1_prop-test/src/App4.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 전역 컴포넌트 테스트 -->
+  <!-- <Checkbox /> -->
+
+  <!-- 지역 컴포넌트 테스트 -->
+  <h1>App 테스트</h1>
+  <ul>
+    <!-- 이 방식은 전달해야할 속성이 많다면, 비효율적일 수 있다! -->
+    <Checkbox v-for="idol in idols" :idol="idol"/>
+  </ul>
+</template>
+
+<script>
+import Checkbox from './components/Checkbox4.vue';
+
+  // 이거 먼저 적으면 위에 거 자동완성 됨.
+  export default {
+    name : 'App4',
+    components : {Checkbox},
+
+    data() {
+      return {
+        idols : [
+            new Idol(1, "BTS", true),
+            new Idol(2, "블핑", false),
+            new Idol(3, "류호근", false),
+            new Idol(4, "제이팍", false),
+        ]
+      }
+    }
+  }
+</script>
+
---
+++ workspace/5_vue-app2/1_prop-test/src/components/Checkbox4.vue
@@ -0,0 +1,22 @@
+<!-- vbase-css -->
+<!-- vue vscode snippets -->
+
+<template>
+    <input type="checkbox" :checked='checked'>{{ id }} - {{ name }}
+    <br>
+</template>
+
+<script>
+// 경고를 띄워주는 걸 보고, 개발자들은 코드를 조기에 수정할 수 있다!
+    import Idol from '../idol.js'
+    export default {
+        name : 'Checkbox',
+        props : {
+            idol : Idol
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
