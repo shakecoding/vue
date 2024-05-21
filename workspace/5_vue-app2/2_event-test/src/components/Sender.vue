a875bbae6e062aca6a0b71f728945fa06c8d7f94
hogeun <114096025+shakecoding@users.noreply.github.com>
2024-05-21 18:43:30 +0900

    5월 21일 todoList 리팩토링

---
+++ workspace/5_vue-app2/2_event-test/src/components/Sender.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="border:solid 1px gray; margin:5px; padding:5px;">
+        <h2>Sender</h2><hr/>
+        <button @click="sendMessage">이벤트 발신</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Sender",
+        methods: {
+            sendMessage() {
+              // 메세지 이벤트, 정보를 전달!
+                this.emitter.emit("message", Date.now() + "에 발신된 메시지");
+            }
+        }
+    }
+</script>
