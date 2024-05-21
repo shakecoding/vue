d4bd252377c288d49847952ab49e830cc8f91bbb
shakecoding <hogeun4656@gmail.com>
2024-05-22 01:59:27 +0900

    5월 22일 자기 전 커밋

--- workspace/5_vue-app2/2_event-test/src/main.js
+++ workspace/5_vue-app2/2_event-test/src/main.js
@@ -1,9 +1,9 @@
 // import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+// import { createApp } from 'vue'
+// import App from './App.vue'
+//
+// createApp(App).mount('#app')
 //
 // import { createApp } from 'vue'
 // // import App from './App.vue'
